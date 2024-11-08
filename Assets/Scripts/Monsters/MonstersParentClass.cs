b1c80f18eee3fc20cf558b328331d86ffffadfa7
highlow12 <77542378+highlow12@users.noreply.github.com>
2024-11-08 22:44:52 +0900

    Enemy movement (#3)
    * Feat: Add IMovement
    
    IMovement 인터페이스를 만듦
    
    * Feat: Add MonstersParentClass
    
    MonstersParentClass.cs 안에 몬스터 최상위 클래스와 그것을 상속받는 NomalMonster, FSM을 이용하는 BossMonster을 만듦
    
    * Update IMovement.cs
    
    * Feat: Make basic movements
    
    기본적인 인터페이스의 사용 방법을 알리기 위해 기본 움직임 2종을 만들었다
    
    * Comment: Add comment
    
    * Feat: Make FSM class
    
    몬스터의 행동 트리를 확장성을 고려해서 관리하기 위해 FSM을 다른 클래스로 분리하였다.
    
    * Feat: Basic Move Monster
    
    IMovement와 NomalMonster를 검증하기 위해 간단한 플레이어를 따라다니는 몬스터를 만들었다.

---
+++ Assets/Scripts/Monsters/MonstersParentClass.cs
@@ -0,0 +1,129 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Monster : MonoBehaviour
+{
+    [SerializeField] protected float maxHealth;
+    [SerializeField] protected float currentHealth;
+    [SerializeField] protected float damage;
+    [SerializeField] protected float defense;
+
+    protected bool isDead;
+    protected Transform playerTransform;
+
+    protected virtual void Start()
+    {
+        currentHealth = maxHealth;
+        playerTransform = GameManager.Instance.player.transform;
+    }
+
+    public virtual void TakeDamage(float damage)
+    {
+        float actualDamage = Mathf.Max(damage - defense, 1);
+        currentHealth -= actualDamage;
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            Die();
+        }
+    }
+
+    protected virtual void Die()
+    {
+        isDead = true;
+        // 사망 처리 로직
+        //DropLoot();
+    }
+
+    //protected abstract void DropLoot();
+    protected abstract void Attack();
+}
+
+// 일반 몬스터 클래스
+public class NormalMonster : Monster
+{
+    [SerializeField] private float moveSpeed;
+    protected IMovement movement/* = Enter Movement*/;
+    
+    protected override void Start()
+    {
+        base.Start();
+        // 일반 몬스터는 한 가지 이동 패턴만 사용
+        if(movement == null){
+            Debug.LogError("Enter Movement");
+            Destroy(this);
+        }
+    }
+
+    private void Update()
+    {
+        if (!isDead)
+        {
+            Move();
+            CheckAttackRange();
+        }
+    }
+
+    private void Move()
+    {
+        Vector2 movementVector = movement.CalculateMovement(
+            transform.position, 
+            playerTransform.position
+        );
+        transform.position += (Vector3)movementVector * Time.deltaTime * moveSpeed;
+    }
+// Monster Attack
+    protected virtual void CheckAttackRange()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+public enum BossState
+{
+    Idle,
+    Chase,
+    Attack1,
+    Attack2,
+    Berserk,
+    StunState
+}
+
+// 보스 몬스터
+
+public class BossMonster : Monster
+{
+    [SerializeField] private float phaseChangeHealthThreshold;
+    [SerializeField] private float berserkHealthThreshold;
+    private Dictionary<BossState, IMovement> stateMovements;
+    private BossState currentState;
+    private float stateTimer;
+
+    // 각 상태별 지속 시간
+    private Dictionary<BossState, float> stateDurations;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+// Boss Take Damage
+    public override void TakeDamage(float damage)
+    {
+        base.TakeDamage(damage);
+        
+        // 보스 특수 피해 처리
+        if (currentHealth <= phaseChangeHealthThreshold)
+        {
+            // 페이즈 변경 로직
+        }
+    }
+
+    protected override void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+}
