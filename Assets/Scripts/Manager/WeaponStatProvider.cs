e47ac9ffe8e1e2aa29e1778d85a8a4cae567255e
Bonobo0 <kjh893197@naver.com>
2024-11-12 19:45:48 +0900

    feat: weapon stat chart

--- Assets/Scripts/Manager/WeaponStatProvider.cs
+++ Assets/Scripts/Manager/WeaponStatProvider.cs
@@ -1,148 +1,92 @@
 using UnityEngine;
 using System.Collections.Generic;
+using BackEnd;
 
 [DefaultExecutionOrder(-100)]
 public class WeaponStatProvider : Singleton<WeaponStatProvider>
 {
     public struct WeaponStat
     {
-        public Dictionary<Weapon.WeaponRare, int> attackDamage;
-        public Dictionary<Weapon.WeaponRare, float> attackSpeed;
-        public Dictionary<Weapon.WeaponRare, float> attackRange;
-        public Dictionary<Weapon.WeaponRare, int> attackTarget;
-        public Dictionary<Weapon.WeaponRare, int> projectileCount;
-        public Dictionary<Weapon.WeaponRare, float> projectileSpeed;
+        public string displayName;
+        public Weapon.WeaponType weaponType;
+        public string displayWeaponRare;
+        public Weapon.WeaponRare weaponRare;
+        public float attackDamage;
+        public float attackSpeed;
+        public string displayWeaponAttackRange;
+        public Weapon.WeaponAttackRange weaponAttackRange;
+        public float attackRange;
+        public string displayWeaponAttackTarget;
+        public Weapon.WeaponAttackTarget weaponAttackTarget;
+        public int attackTarget;
+        public int projectileCount;
+        public float projectileSpeed;
     }
-    public Dictionary<Weapon.WeaponType, WeaponStat> weaponsStat = new();
+    public List<WeaponStat> weaponStats = new();
 
+
+    private void GetCurrentWeaponStatChart()
+    {
+        // Get latest weapon stat chart file id
+
+        Where where = new();
+        where.Equal("columnType", "currentWeaponStatChartFileId");
+        var chartFileIdBro = Backend.GameData.Get("Global", where);
+        if (!chartFileIdBro.IsSuccess())
+        {
+            Debug.LogError("Get currentWeaponStatChartFileId failed");
+            return;
+        }
+        if (chartFileIdBro.GetReturnValuetoJSON()["rows"].Count <= 0)
+        {
+            Debug.LogError("No currentWeaponStatChartFileId data");
+            return;
+        }
+        string currentWeaponStatChartFileId = chartFileIdBro.Rows()[0]["data"]["N"].ToString();
+
+        // Get weapon stat chart
+
+        var chartBro = Backend.Chart.GetChartContents(currentWeaponStatChartFileId);
+        if (!chartBro.IsSuccess())
+        {
+            Debug.LogError("Get weapon stat chart failed");
+            Debug.LogError(chartBro);
+            return;
+        }
+        Debug.Log("Get weapon stat chart success");
+        LitJson.JsonData chartData = chartBro.GetReturnValuetoJSON()["rows"];
+        weaponStats.Clear();
+
+        // Set weapon stats
+
+        for (int i = 0; i < chartData.Count; i++)
+        {
+            WeaponStat weaponStat = new()
+            {
+                displayName = chartData[i]["displayName"]["S"].ToString(),
+                weaponType = (Weapon.WeaponType)int.Parse(chartData[i]["weaponType"]["S"].ToString()),
+                displayWeaponRare = chartData[i]["displayWeaponRare"]["S"].ToString(),
+                weaponRare = (Weapon.WeaponRare)int.Parse(chartData[i]["weaponRare"]["S"].ToString()),
+                attackDamage = float.Parse(chartData[i]["attackDamage"]["S"].ToString()),
+                attackSpeed = float.Parse(chartData[i]["attackSpeed"]["S"].ToString()),
+                displayWeaponAttackRange = chartData[i]["displayWeaponAttackRange"]["S"].ToString(),
+                weaponAttackRange = (Weapon.WeaponAttackRange)int.Parse(chartData[i]["weaponAttackRange"]["S"].ToString()),
+                attackRange = float.Parse(chartData[i]["attackRange"]["S"].ToString()),
+                displayWeaponAttackTarget = chartData[i]["displayWeaponAttackTarget"]["S"].ToString(),
+                weaponAttackTarget = (Weapon.WeaponAttackTarget)int.Parse(chartData[i]["weaponAttackTarget"]["S"].ToString()),
+                attackTarget = int.Parse(chartData[i]["attackTarget"]["S"].ToString()),
+                projectileCount = int.Parse(chartData[i]["projectileCount"]["S"].ToString()),
+                projectileSpeed = float.Parse(chartData[i]["projectileSpeed"]["S"].ToString())
+            };
+            weaponStats.Add(weaponStat);
+        }
+
+    }
 
     // Set weapon stats
     private new void Awake()
     {
-        WeaponStat shotgunStat = new()
-        {
-            attackDamage = new Dictionary<Weapon.WeaponRare, int>
-            {
-                {Weapon.WeaponRare.Common, 10 },
-                {Weapon.WeaponRare.Uncommon, 15 },
-                {Weapon.WeaponRare.Rare, 20 },
-                {Weapon.WeaponRare.Epic, 25 },
-                {Weapon.WeaponRare.Legendary, 30 }
-            },
-            attackSpeed = new Dictionary<Weapon.WeaponRare, float>
-            {
-                {Weapon.WeaponRare.Common, 1 },
-                {Weapon.WeaponRare.Uncommon, 2 },
-                {Weapon.WeaponRare.Rare, 3 },
-                {Weapon.WeaponRare.Epic, 4 },
-                {Weapon.WeaponRare.Legendary, 5 }
-            },
-            attackRange = new Dictionary<Weapon.WeaponRare, float>
-            {
-                {Weapon.WeaponRare.Common, 10 },
-                {Weapon.WeaponRare.Uncommon, 12 },
-                {Weapon.WeaponRare.Rare, 14 },
-                {Weapon.WeaponRare.Epic, 16 },
-                {Weapon.WeaponRare.Legendary, 18 }
-            },
-            projectileCount = new Dictionary<Weapon.WeaponRare, int>
-            {
-                {Weapon.WeaponRare.Common, 7 },
-                {Weapon.WeaponRare.Uncommon, 8 },
-                {Weapon.WeaponRare.Rare, 9 },
-                {Weapon.WeaponRare.Epic, 10 },
-                {Weapon.WeaponRare.Legendary, 12 }
-            },
-            projectileSpeed = new Dictionary<Weapon.WeaponRare, float>
-            {
-                {Weapon.WeaponRare.Common, 3 },
-                {Weapon.WeaponRare.Uncommon, 4 },
-                {Weapon.WeaponRare.Rare, 5 },
-                {Weapon.WeaponRare.Epic, 6 },
-                {Weapon.WeaponRare.Legendary, 7 }
-            }
-        };
-        WeaponStat axeStat = new()
-        {
-            attackDamage = new Dictionary<Weapon.WeaponRare, int>
-            {
-                {Weapon.WeaponRare.Common, 100 },
-                {Weapon.WeaponRare.Uncommon, 150 },
-                {Weapon.WeaponRare.Rare, 200 },
-                {Weapon.WeaponRare.Epic, 250 },
-                {Weapon.WeaponRare.Legendary, 300 }
-            },
-            attackSpeed = new Dictionary<Weapon.WeaponRare, float>
-            {
-                {Weapon.WeaponRare.Common, 1 },
-                {Weapon.WeaponRare.Uncommon, 2 },
-                {Weapon.WeaponRare.Rare, 3 },
-                {Weapon.WeaponRare.Epic, 4 },
-                {Weapon.WeaponRare.Legendary, 5 }
-            },
-            attackRange = new Dictionary<Weapon.WeaponRare, float>
-            {
-                {Weapon.WeaponRare.Common, 0.5f },
-                {Weapon.WeaponRare.Uncommon, 1.0f },
-                {Weapon.WeaponRare.Rare, 1.5f },
-                {Weapon.WeaponRare.Epic, 2.0f },
-                {Weapon.WeaponRare.Legendary, 2.5f }
-            },
-            attackTarget = new Dictionary<Weapon.WeaponRare, int>
-            {
-                {Weapon.WeaponRare.Common, 1 },
-                {Weapon.WeaponRare.Uncommon, 1 },
-                {Weapon.WeaponRare.Rare, 1 },
-                {Weapon.WeaponRare.Epic, 1 },
-                {Weapon.WeaponRare.Legendary, 1 }
-            }
-        };
-        WeaponStat holyWaterStat = new()
-        {
-            attackDamage = new Dictionary<Weapon.WeaponRare, int>
-            {
-                {Weapon.WeaponRare.Common, 10 },
-                {Weapon.WeaponRare.Uncommon, 15 },
-                {Weapon.WeaponRare.Rare, 20 },
-                {Weapon.WeaponRare.Epic, 25 },
-                {Weapon.WeaponRare.Legendary, 30 }
-            },
-            attackSpeed = new Dictionary<Weapon.WeaponRare, float>
-            {
-                {Weapon.WeaponRare.Common, 1 },
-                {Weapon.WeaponRare.Uncommon, 1.2f },
-                {Weapon.WeaponRare.Rare, 1.5f },
-                {Weapon.WeaponRare.Epic, 1.8f },
-                {Weapon.WeaponRare.Legendary, 2.1f }
-            },
-            attackRange = new Dictionary<Weapon.WeaponRare, float>
-            {
-                {Weapon.WeaponRare.Common, 1 },
-                {Weapon.WeaponRare.Uncommon, 1.5f },
-                {Weapon.WeaponRare.Rare, 2 },
-                {Weapon.WeaponRare.Epic, 2.5f },
-                {Weapon.WeaponRare.Legendary, 3f }
-            },
-            projectileCount = new Dictionary<Weapon.WeaponRare, int>
-            {
-                {Weapon.WeaponRare.Common, 1 },
-                {Weapon.WeaponRare.Uncommon, 1 },
-                {Weapon.WeaponRare.Rare, 1 },
-                {Weapon.WeaponRare.Epic, 1 },
-                {Weapon.WeaponRare.Legendary, 1 }
-            },
-            projectileSpeed = new Dictionary<Weapon.WeaponRare, float>
-            {
-                {Weapon.WeaponRare.Common, 5 },
-                {Weapon.WeaponRare.Uncommon, 6 },
-                {Weapon.WeaponRare.Rare, 7 },
-                {Weapon.WeaponRare.Epic, 8 },
-                {Weapon.WeaponRare.Legendary, 9 }
-            }
-        };
-        weaponsStat.Add(Weapon.WeaponType.Shotgun, shotgunStat);
-        weaponsStat.Add(Weapon.WeaponType.Axe, axeStat);
-        weaponsStat.Add(Weapon.WeaponType.HolyWater, holyWaterStat);
+        GetCurrentWeaponStatChart();
     }
 
 
