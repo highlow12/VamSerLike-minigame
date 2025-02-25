873ea73bd69b0d6d1db90a10021a5d7fe54c2d32
Bonobo0 <kjh893197@naver.com>
2025-02-25 22:30:37 +0900

    feat: test ui system

--- Assets/Scripts/Test/SkinSelectTestUi.cs
+++ Assets/Scripts/Test/SkinSelectTestUi.cs
@@ -4,20 +4,6 @@ public class SkinSelectTestUi : MonoBehaviour
 {
     public void SkinSelectGUI()
     {
-        GUIStyle titleStyle = new GUIStyle
-        {
-            alignment = TextAnchor.MiddleCenter,
-            fontSize = 20,
-            normal = { background = Texture2D.whiteTexture, textColor = Color.black },
-        };
-
-        Texture2D backgroundTexture = new Texture2D(1, 1);
-        backgroundTexture.SetPixel(0, 0, Color.gray);
-        backgroundTexture.Apply();
-        titleStyle.normal.background = backgroundTexture;
-
-        GUILayout.Label("Skin Select", titleStyle);
-
         // Load all skin data
         PlayerSkinSO[] skinData = Resources.LoadAll<PlayerSkinSO>("ScriptableObjects/Player/Skin");
 
