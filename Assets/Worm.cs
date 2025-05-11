11176d7c5c4d660b09bd39aa71fa4cc25a144606
Hyeon <ink0416@naver.com>
2025-05-11 16:31:11 +0900

    WIP: Stage3 VFX
    벌레가 위에서 아래로 기어가는 효과 구현

---
+++ Assets/Worm.cs
@@ -0,0 +1,89 @@
+using System.Collections.Generic;
+using DG.Tweening;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class Worm : MonoBehaviour
+{
+    public bool isMain = true;
+    public int maxWormCount = 10;
+    [SerializeField] private GameObject worm;
+    List<GameObject> worms = new List<GameObject>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        WormReset();
+    }
+
+    void Awake()
+    {
+        WormReset();
+    }
+
+    void OnEnable()
+    {
+        WormReset();
+    }
+
+    void WormReset()
+    {
+        if (isMain) { worms.Add(worm); return; }
+        
+        Vector3 randomPos = Camera.main.ViewportToWorldPoint(new Vector3(Random.Range(0f, 1f), 1.2f));
+        transform.position = new Vector3(randomPos.x, randomPos.y, 0);
+
+        // top to bottom
+        transform.DOLocalMoveY(-14, 1f).SetEase(Ease.Linear).OnComplete(() =>
+        {
+            // disable the game object
+            gameObject.SetActive(false);
+        });
+    }
+
+    public void test()
+    {
+        int activeWormCount = GetActiveWormCount();
+        if (activeWormCount >= maxWormCount) return;
+        else if (worms.Count <= maxWormCount)
+        {
+            GameObject clone = Instantiate(worm, transform);
+            Worm _ = clone.GetComponent<Worm>();
+            _.isMain = false;
+            worms.Add(clone);
+            clone.SetActive(true);
+        }
+        else if (worms.Count - activeWormCount > 0)
+        {
+            GameObject inactiveWorm = GetInactiveWorm();
+            inactiveWorm.SetActive(true);
+        }
+    }
+
+    int GetActiveWormCount()
+    {
+        return worms.FindAll((x) => x.activeSelf).Count;
+    }
+
+    GameObject GetInactiveWorm()
+    {
+        return worms.Find((x) => !x.activeSelf);
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(Worm))]
+public class WormInspector: Editor
+{
+  public override void OnInspectorGUI()
+  {
+    base.OnInspectorGUI();
+    var worm = target as Worm;
+    if (GUILayout.Button("Test"))
+    {
+        worm.test();
+    }
+  }
+}
+#endif
