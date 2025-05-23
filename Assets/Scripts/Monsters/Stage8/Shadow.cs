417989d45fcd65df87fc9b65305c786617dde180
highlow12 <77542378+highlow12@users.noreply.github.com>
2025-05-23 15:30:45 +0900

    Add new sprite asset for Stage 8: 그림자인형 이동9.png with metadata

---
+++ Assets/Scripts/Monsters/Stage8/Shadow.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Shadow : NormalMonster
+{
+    public float amplitude = 1f; // 진폭
+    public float frequency = 1f; // 주기
+
+    protected override void Start()
+    {
+        movement = new SinMovement(amplitude, frequency, moveSpeed);
+        base.Start();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (collision.TryGetComponent<Player>(out var player))
+            {
+                //player.TakeDamage(damage);
+            }
+        }
+    }
+}
